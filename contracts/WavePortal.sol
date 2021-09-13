236a154fbc37b14df5b5586aa732dbef3bddf140
kylestratis <kyle.stratis@gmail.com>
2021-09-13 00:39:10 -0400

    Section 4 part 2

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint totalWaves;
+    uint private seed;
     
     event NewWave(address indexed from, uint timestamp, string message);
 
@@ -25,13 +26,20 @@ contract WavePortal {
         totalWaves += 1;
         console.log("%s saved with message %s", msg.sender, _message);
         waves.push(Wave(msg.sender, _message, block.timestamp));
+        
+        uint randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %s", randomNumber);
+        seed = randomNumber;
+        if(randomNumber < 50) {
+            console.log("%s won!", msg.sender);
+            uint prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more money than c ontract has.");
+            (bool success,) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract");
+        }
         emit NewWave(msg.sender, block.timestamp, _message);
 
-        uint prizeAmount = 0.0001 ether;
-        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than c ontract has.");
-        (bool success,) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract");
-    }
+            }
 
     function getAllWaves() view public returns (Wave[] memory) {
         return waves;
