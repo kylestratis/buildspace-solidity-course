539cf46b31269fa165473b8acbf9a1fc53c155bd
kylestratis <kyle.stratis@gmail.com>
2021-09-13 00:17:24 -0400

    Section 4

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,13 +6,35 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint totalWaves;
-    constructor () {
-        console.log("yo yo I'm a contract and I'm smart");
+    
+    event NewWave(address indexed from, uint timestamp, string message);
+
+    struct Wave {
+        address waver;
+        string message;
+        uint timestamp;
     }
 
-    function wave() public {
+    Wave[] waves; // Hold all the wave structs that people send!
+
+    constructor() payable {
+        console.log("Constructed");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
-        console.log("%s is waved!", msg.sender);
+        console.log("%s saved with message %s", msg.sender, _message);
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
+
+        uint prizeAmount = 0.0001 ether;
+        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than c ontract has.");
+        (bool success,) = (msg.sender).call{value: prizeAmount}("");
+        require(success, "Failed to withdraw money from contract");
+    }
+
+    function getAllWaves() view public returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() view public returns (uint) {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,10 +4,11 @@ async function main() {
     console.log("Deploying contracts with the account: ", deployer.address);
     console.log("Account balance: ", (await deployer.getBalance()).toString());
 
-    const Token = await hre.ethers.getContractFactory("WavePortal");
-    const token = await Token.deploy()
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy({value: ethers.utils.parseEther("0.1")});
+    await waveContract.deployed()
 
-    console.log("WavePortal address: ", token.address);
+    console.log("WavePortal address: ", waveContract.address);
 }
 
 main()
